118c0f98f233c9aaf33ce3f08b94a26ab65b4589
Haeszm <99459243+Haeszm@users.noreply.github.com>
2023-11-14 01:25:14 +0200

    Add files via upload

--- config.py
+++ config.py
@@ -78,7 +78,7 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://telegra.ph/file/6dff19491145b93d0fb09.jpg"
+    "START_IMG_URL", "https://telegra.ph/file/1efdc3c274832806953f2.jpg"
 )
 PING_IMG_URL = getenv(
     "PING_IMG_URL", "https://te.legra.ph/file/b8a0c1a00db3e57522b53.jpg"
